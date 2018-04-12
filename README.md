41f9c40367dc1fa396a51881dc6c9f614be68b09
Unknown <3103204417@qq.com>
2018-04-12 08:00:29 +0800

    hello,ARM
    hello,arm

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-ARM_learning
+# ARM_learning
+
+learn how to program an ARM STM32 Cortex M3 microcontroller in embedded C and ARM assembly language under Ubuntu. 
+
+## Reference
+
+http://fab.cba.mit.edu/classes/863.13/tutorials/EmbeddedProgramming/stm32.html
+
+http://academy.cba.mit.edu/classes/embedded_programming/index.html
+
+http://fun-tech.se/stm32/gcc/index.php
---
+++ doc/install/install_toolchain.sh
@@ -0,0 +1,10 @@
+sudo apt-get install  gcc-5-arm-linux-gnueabihf 
+sudo apt-get install  gcc-arm-linux-gnueabi
+sudo apt-get install  gcc-arm-linux-gnueabihf
+# http://fun-tech.se/stm32/OpenOCD/index.php
+sudo apt-get install openocd
+# wget http://openocd.berlios.de/doc/pdf/openocd.pdf
+sudo apt-get install qemu-system-arm
+sudo apt-get install qemu-user-static 
+sudo apt install gdb-multiarch
+sudo apt-get install u-boot-tools 
