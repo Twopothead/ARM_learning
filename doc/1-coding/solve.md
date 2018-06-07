7fb02719b7048da5c21dd2c6421141c1fc0fad58
Unknown <3103204417@qq.com>
2018-06-07 08:48:28 +0800

    a Big Bug fixed
    a Big Bug fixed

--- doc/1-coding/solve.md
+++ doc/1-coding/solve.md
@@ -9,3 +9,88 @@ sudo apt-get install enca
 enca -L zh_CN -x utf-8 *
 ```
 
+https://stackoverflow.com/questions/231229/how-to-generate-a-makefile-with-source-in-sub-directories-using-just-one-makefil
+
+https://blog.csdn.net/HMSIWTV/article/details/20905177
+
+https://stackoverflow.com/questions/13897945/wildcard-to-obtain-list-of-all-directories
+
+https://wiki.osdev.org/Makefile
+
+https://github.com/sczh01/lnx_drv/blob/d90bd6730423988c7586e5423bacd9cb15f3fa19/Makefile.bak
+
+https://www.cnblogs.com/catgatp/p/6534249.html
+
+### wildcard-to-obtain-list-of-all-directories
+
+Use `sort` and `dir` functions together with `wildcard`:
+
+```
+DIRECTORY = $(sort $(dir $(wildcard ../Test/*/)))
+
+```
+
+From GNU make manual:
+
+> $(dir names...) Extracts the directory-part of each file name in names. The directory-part of the file name is everything up through (and including) the last slash in it. If the file name contains no slash, the directory part is the string ‘./’.
+>
+> $(sort list) Sorts the words of list in lexical order, removing duplicate words. The output is a list of words separated by single spaces.
+
+http://people.ece.cornell.edu/land/courses/ece4760/
+
+https://stackoverflow.com/questions/1932904/make-wildcard-subdirectory-targets
+
+### ST-link用法
+
+https://steward-fu.github.io/website/mcu/stm32f103/n900_stlink-v2_flash.htm
+
+https://github.com/steward-fu
+
+## Undefined references with Newlib
+
+newlib/libc/reent/writer.c:58: undefined reference to `_write'
+
+The Code Sourcery Lite tool chain is provided with the [newlib](http://sourceware.org/newlib/) C Library from Redhat. Because this is an embedded toolchain some stub functions known as System Functions must be provided by the embedded system that would normally be provided by a host operating system.
+
+The is shown by the fact that if you try to compile a C program that uses printf you will see the following error message:
+
+ n function `_write_r':writer.c:(.text+0x16): undefined reference to `_write'
+
+解决办法就是在main函数中删去printf
+
+  // printf("\r\n");
+  // printf("nes emulator for stm32f103tb\r\n");
+
+删去printf
+
+https://blog.csdn.net/zhengyangliu123/article/details/54966402
+
+这里需要大家明白的是，GNU C 与 KEIL C 下的标准库函数实际上都是各个不同的机构组织编写的，虽然他们符合不同时期的C标准，如C89、C99等，那也只是用户层的API相同（同时要明白他们这些标准库是属于编译器的一部分的，就储存在编译器路径下的lib文件夹中）。虽然上层被调用的标准C函数相同，但是他们各有各的实现方式，他们在底层实现是可能完全不同的样子。所以在我们更换工具链后，一定要注意自己工程中的代码不一定会适应新的工具链开发环境。
+
+我之前使用的是Keil环境的usart而GNU对usart实现并不一样，需要用GNU的方法实现usart
+
+http://www.openstm32.org/forumthread1055
+
+https://electronics.stackexchange.com/questions/206113/how-do-i-use-the-printf-function-on-stm32
+
+http://www.exodusamps.com/Exodusamps/Embedded_Stuff/Entries/2012/2/8_Use_printf()_on_STM32_in_CoIDE.html
+
+keyword: stm32 gnu remap printf
+
+> The way I got printf (and all other console-oriented stdio functions) to work was by creating custom implementations of the low-level I/O functions like `_read()` and `_write()`.
+>
+> The GCC C library makes calls to the following functions to perform low-level I/O :
+>
+> ```
+> int _read(int file, char *data, int len)
+> int _write(int file, char *data, int len)
+> int _close(int file)
+> int _lseek(int file, int ptr, int dir)
+> int _fstat(int file, struct stat *st)
+> int _isatty(int file)
+>
+> ```
+>
+> These functions are implemented witihin the GCC C library as stub routines with "weak" linkage. If a declaration of any of the above functions appears in your own code, your substitute routine will override the declaration in the library and be used instead of the default (non-functional) routine.
+
+I used STM32CubeMX to setup USART1 (`huart1`) as a serial port. Since I only wanted `printf()`, I only needed to populate the `_write()` function, which I did as follows. This is conventionally contained in `syscalls.c`.
---
+++ src/01_Coding/blink/src/main.c
@@ -0,0 +1,226 @@
+//
+// This file is part of the GNU ARM Eclipse distribution.
+// Copyright (c) 2014 Liviu Ionescu.
+//
+#include "stm32f10x.h"
+
+#include "sys.h"
+#include "delay.h"
+#include "usart.h"
+#include "led.h"
+
+#include "led.h"
+#include "delay.h"
+#include "key.h"
+#include "sys.h"
+#include "lcd.h"
+#include "usart.h"	 
+#include "24cxx.h"
+#include "w25qxx.h"
+#include "touch.h"
+
+
+
+ 
+
+static u8 fac_us = 0;  //us延时倍乘数
+static u16 fac_ms = 0; //ms
+
+/************************************************
+ ALIENTEK战舰STM32开发板实验27
+ 触摸屏 实验 
+ 技术支持：www.openedv.com
+ 淘宝店铺：http://eboard.taobao.com 
+ 关注微信公众平台微信号："正点原子"，免费获取STM32资料。
+ 广州市星翼电子科技有限公司  
+ 作者：正点原子 @ALIENTEK
+************************************************/
+
+
+
+//清空屏幕并在右上角显示"RST"
+void Load_Drow_Dialog(void)
+{
+	LCD_Clear(WHITE);	//清屏   
+ 	POINT_COLOR=BLUE;	//设置字体为蓝色 
+	LCD_ShowString(lcddev.width-24,0,200,16,16,"RST");//显示清屏区域
+  	POINT_COLOR=RED;	//设置画笔蓝色 
+}
+////////////////////////////////////////////////////////////////////////////////
+//电容触摸屏专有部分
+//画水平线
+//x0,y0:坐标
+//len:线长度
+//color:颜色
+void gui_draw_hline(u16 x0,u16 y0,u16 len,u16 color)
+{
+	if(len==0)return;
+	LCD_Fill(x0,y0,x0+len-1,y0,color);	
+}
+//画实心圆
+//x0,y0:坐标
+//r:半径
+//color:颜色
+void gui_fill_circle(u16 x0,u16 y0,u16 r,u16 color)
+{											  
+	u32 i;
+	u32 imax = ((u32)r*707)/1000+1;
+	u32 sqmax = (u32)r*(u32)r+(u32)r/2;
+	u32 x=r;
+	gui_draw_hline(x0-r,y0,2*r,color);
+	for (i=1;i<=imax;i++) 
+	{
+		if ((i*i+x*x)>sqmax)// draw lines from outside  
+		{
+ 			if (x>imax) 
+			{
+				gui_draw_hline (x0-i+1,y0+x,2*(i-1),color);
+				gui_draw_hline (x0-i+1,y0-x,2*(i-1),color);
+			}
+			x--;
+		}
+		// draw lines from inside (center)  
+		gui_draw_hline(x0-x,y0+i,2*x,color);
+		gui_draw_hline(x0-x,y0-i,2*x,color);
+	}
+}  
+//两个数之差的绝对值 
+//x1,x2：需取差值的两个数
+//返回值：|x1-x2|
+u16 my_abs(u16 x1,u16 x2)
+{			 
+	if(x1>x2)return x1-x2;
+	else return x2-x1;
+}  
+//画一条粗线
+//(x1,y1),(x2,y2):线条的起始坐标
+//size：线条的粗细程度
+//color：线条的颜色
+void lcd_draw_bline(u16 x1, u16 y1, u16 x2, u16 y2,u8 size,u16 color)
+{
+	u16 t; 
+	int xerr=0,yerr=0,delta_x,delta_y,distance; 
+	int incx,incy,uRow,uCol; 
+	if(x1<size|| x2<size||y1<size|| y2<size)return; 
+	delta_x=x2-x1; //计算坐标增量 
+	delta_y=y2-y1; 
+	uRow=x1; 
+	uCol=y1; 
+	if(delta_x>0)incx=1; //设置单步方向 
+	else if(delta_x==0)incx=0;//垂直线 
+	else {incx=-1;delta_x=-delta_x;} 
+	if(delta_y>0)incy=1; 
+	else if(delta_y==0)incy=0;//水平线 
+	else{incy=-1;delta_y=-delta_y;} 
+	if( delta_x>delta_y)distance=delta_x; //选取基本增量坐标轴 
+	else distance=delta_y; 
+	for(t=0;t<=distance+1;t++ )//画线输出 
+	{  
+		gui_fill_circle(uRow,uCol,size,color);//画点 
+		xerr+=delta_x ; 
+		yerr+=delta_y ; 
+		if(xerr>distance) 
+		{ 
+			xerr-=distance; 
+			uRow+=incx; 
+		} 
+		if(yerr>distance) 
+		{ 
+			yerr-=distance; 
+			uCol+=incy; 
+		} 
+	}  
+}   
+////////////////////////////////////////////////////////////////////////////////
+ //5个触控点的颜色												 
+const u16 POINT_COLOR_TBL[CT_MAX_TOUCH]={RED,GREEN,BLUE,BROWN,GRED};  
+//电阻触摸屏测试函数
+void rtp_test(void)
+{
+	u8 key;
+	u8 i=0;	  
+	while(1)
+	{
+	 	key=KEY_Scan(0);
+		tp_dev.scan(0); 		 
+		if(tp_dev.sta&TP_PRES_DOWN)			//触摸屏被按下
+		{	
+		 	if(tp_dev.x[0]<lcddev.width&&tp_dev.y[0]<lcddev.height)
+			{	
+				if(tp_dev.x[0]>(lcddev.width-24)&&tp_dev.y[0]<16)Load_Drow_Dialog();//清除
+				else TP_Draw_Big_Point(tp_dev.x[0],tp_dev.y[0],RED);		//画图	  			   
+			}
+		}else delay_ms(10);	//没有按键按下的时候 	    
+		if(key==KEY0_PRES)	//KEY0按下,则执行校准程序
+		{
+			LCD_Clear(WHITE);//清屏
+		    TP_Adjust();  	//屏幕校准  
+			Load_Drow_Dialog();
+		}
+		i++;
+		if(i%20==0)LED0=!LED0;
+	}
+}
+//电容触摸屏测试函数
+void ctp_test(void)
+{
+	u8 t=0;
+	u8 i=0;	  	    
+ 	u16 lastpos[5][2];		//记录最后一次的数据 
+	while(1)
+	{
+		tp_dev.scan(0);
+		for(t=0;t<CT_MAX_TOUCH;t++)
+		{
+			if((tp_dev.sta)&(1<<t))
+			{
+				if(tp_dev.x[t]<lcddev.width&&tp_dev.y[t]<lcddev.height)
+				{
+					if(lastpos[t][0]==0XFFFF)
+					{
+						lastpos[t][0] = tp_dev.x[t];
+						lastpos[t][1] = tp_dev.y[t];
+					}
+					lcd_draw_bline(lastpos[t][0],lastpos[t][1],tp_dev.x[t],tp_dev.y[t],2,POINT_COLOR_TBL[t]);//画线
+					lastpos[t][0]=tp_dev.x[t];
+					lastpos[t][1]=tp_dev.y[t];
+					if(tp_dev.x[t]>(lcddev.width-24)&&tp_dev.y[t]<16)
+					{
+						Load_Drow_Dialog();//清除
+					}
+				}
+			}else lastpos[t][0]=0XFFFF;
+		}
+		
+		delay_ms(5);i++;
+		if(i%20==0)LED0=!LED0;
+	}	
+}
+
+
+
+
+ int main(void)
+ {	
+ 		    
+	delay_init();	    	 //延时函数初始化	  
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置中断优先级分组为组2：2位抢占优先级，2位响应优先级
+	uart_init(115200);	 	//串口初始化为115200
+	 
+ 	// LED_Init();			     //LED端口初始化
+	LCD_Init();	
+	// KEY_Init();	 	
+ // 	tp_dev.init();
+	 
+ 	// POINT_COLOR=RED;//设置字体为红色 
+	// LCD_ShowString(60,50,200,16,16,"WarShip STM32");	
+	// LCD_ShowString(60,70,200,16,16,"TOUCH TEST");	
+	// LCD_ShowString(60,90,200,16,16,"ATOM@ALIENTEK");
+	// LCD_ShowString(60,110,200,16,16,"2015/1/15");
+ //   	LCD_ShowString(60,130,200,16,16,"Press KEY0 to Adjust");	
+ //   	if(tp_dev.touchtype!=0XFF)LCD_ShowString(60,130,200,16,16,"Press KEY0 to Adjust");//电阻屏才显示
+	delay_ms(1500);
+	Load_Drow_Dialog();	 	
+	if(tp_dev.touchtype&0X80)ctp_test();	//电容屏测试
+	else rtp_test(); 			
+ }
